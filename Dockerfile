abb22afde5064ce9eaeaf92969949751f31f4813
Khayati Diae <khayatidiae67@gmail.com>
2025-07-07 16:52:36 +0100

    update dockerfile and api
    adding transformers to dockerfile and setting up api

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ COPY ./ /app
 
 RUN pip install -r requirements.txt
 
+RUN apt-get update && apt-get install -y git
+
 CMD fastapi run --reload --host=0.0.0.0 --port=7860
--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("text2text-generation", model="google/flan-t5-base")
 
 app = FastAPI()
 
+
 @app.get('/')
 def home():
-    return {"hello" : "Khayati Diae"}
+    return {"hello": "Khayati Diae"}
+
+
+@app.get('/ask')
+def ask(prompt: str):
+    result = pipe(prompt)
+    return result[0]
